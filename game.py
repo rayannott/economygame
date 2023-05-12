943204a85ab24cc3b1390b67518d1781bd1f73a9
Airat <imjobjob@gmail.com>
2023-05-12 15:57:47 +0200

    basic game gui: completed panels

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import pygame
 
 from player import Player
 from gui.gui_rect import Button, Label, ProgressBar, Panel
-from gui.pygame_utils import BLACK, FONT_NORM, FRAMERATE, WHITE, WINDOW_SIZE, FONT_HUGE, FONT_HINT, CP0
+from gui.pygame_utils import BLACK, FONT_NORM, FRAMERATE, WHITE, WINDOW_SIZE, FONT_HUGE, FONT_SMALL, CP0
 from utils import TICK
 
 
@@ -37,17 +37,30 @@ class Game:
         self.info_panel = Panel((3, 3), (300, 240), self.surface, 'info')
         self.info_panel.add_labels(
             [
-                Label(f'time: {self.times_ticked} tx', self.surface, FONT_NORM, WHITE, topleft=(6, 3)),
-                Label(f'balance: {self.player.balance}', self.surface, FONT_NORM, WHITE, topleft=(6, 28)),
-                Label(f'mpt: {self.player.mpt}', self.surface, FONT_NORM, WHITE, topleft=(6, 53)),
-                Label(f'ppt: {self.player.ppt}', self.surface, FONT_NORM, WHITE, topleft=(6, 78)),
+                Label('Info', self.surface, FONT_NORM, CP0[1], topleft=(45, 3)),
+                Label(f'time: {self.times_ticked} tx', self.surface, FONT_NORM, WHITE, topleft=(6, 28)),
+                Label(f'balance: {self.player.balance:.1f}', self.surface, FONT_NORM, WHITE, topleft=(6, 53)),
+                Label(f'[until victory: {self.player.time_until_victory():.0f} tx]', self.surface, FONT_SMALL, WHITE, topleft=(15, 78)),
+                Label(f'mpt: {self.player.mpt}', self.surface, FONT_NORM, WHITE, topleft=(6, 103)),
+                Label(f'ppt: {self.player.ppt}', self.surface, FONT_NORM, WHITE, topleft=(6, 128)),
             ]
         )
         self.info_panel.populate_one(
             'pause_btn',
-            Button((40, 200), (100, 24), self.surface, 'pause', 'toggle pause/unpause')
+            Button((40, 200), (100, 24), self.surface, 'pause', 'toggle pause/unpause', parent=self.info_panel)
         )
+        
 
+        self.inventory_panel = Panel((3, 246), (300, WINDOW_SIZE[1] - 268), self.surface, 'inv')
+        self.inventory_panel.add_labels([
+            Label('Inventory', self.surface, FONT_NORM, CP0[1], topleft=(45, 3))
+        ])
+
+
+        self.shop_panel = Panel((306, 3), (WINDOW_SIZE[0] - 310, WINDOW_SIZE[1] - 25), self.surface, 'shop')
+        self.shop_panel.add_labels(
+            [Label('Shop', self.surface, FONT_NORM, CP0[1], topleft=(45, 3))]
+        )
 
     def update(self):
         '''Updates the brains of the game (back)'''
@@ -57,7 +70,9 @@ class Game:
             self.time_of_last_tick = self.current_time
         
         # labels:
-        self.info_panel.labels[0].set_text(f'time: {self.times_ticked} tx')
+        self.info_panel.labels[1].set_text(f'time: {self.times_ticked} tx')
+        self.info_panel.labels[2].set_text(f'balance: {self.player.balance:.1f}')
+        self.info_panel.labels[3].set_text(f'[until victory: {self.player.time_until_victory():.0f} tx]')
 
     def run(self):
         '''
@@ -72,6 +87,8 @@ class Game:
             pos = pygame.mouse.get_pos()
 
             self.info_panel.update(pos)
+            self.inventory_panel.update(pos)
+            self.shop_panel.update(pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -89,3 +106,4 @@ class Game:
         This function is executed every TICK
         '''
         self.times_ticked += 1
+        self.player.tick()
