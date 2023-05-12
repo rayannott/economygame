83ff0c0186d1f5b85ac711616e8540b677b5a42f
Airat <imjobjob@gmail.com>
2023-05-12 14:31:59 +0200

    rename mpt ppt; new var TICK

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 # game constants:
 
 INITIAL_BALANCE = 1000
-INITIAL_MPS = 0.2
-INITIAL_PPS = 0.1 # in per cent
+INITIAL_MPT = 0.2
+INITIAL_PPT = 0.1 # in per cent
 GOAL_BALANCE = 3000
 COST_INCREASE_PER_TICK = 0.1
+
+TICK = 1. # tick every TICK seconds
